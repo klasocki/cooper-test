ffe055d927c09845a96d3453aacb378cab1eb9bc
klasocki <karolasocki@gmail.com>
2019-11-18 13:08:54 +0200

    Google maps template activity

---
+++ app/src/main/java/com/example/coopertest/MapsActivity.kt
@@ -0,0 +1,34 @@
+package com.example.coopertest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val joensuu = LatLng(62.6, 29.76)
+        mMap.addMarker(MarkerOptions().position(joensuu).title("Marker in Joensuu"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(joensuu))
+    }
+}
