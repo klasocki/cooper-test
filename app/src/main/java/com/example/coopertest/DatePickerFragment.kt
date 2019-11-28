4239453414bc1b5332546b3806614864f52c83de
NicolasQuenon <nico_quenon@hotmail.fr>
2019-11-28 12:14:33 +0200

    First version of settings : -Name -Use Miles or metters
    Birthday and gender don't work currently

---
+++ app/src/main/java/com/example/coopertest/DatePickerFragment.kt
@@ -0,0 +1,26 @@
+package com.example.coopertest
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener  {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Use the current date as the default date in the picker
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        // Create a new instance of DatePickerDialog and return it
+        return DatePickerDialog(activity!!.baseContext, this, year, month, day)
+    }
+
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+        // Do something with the date chosen by the user
+    }
+}
--- app/src/main/java/com/example/coopertest/MainActivity.kt
+++ app/src/main/java/com/example/coopertest/MainActivity.kt
@@ -16,14 +16,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
     }
 
     fun startTest(v: View) {
         val intent = Intent(this, TestActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun settings(v: View) {
+        val intent = Intent(this, SettingsActivity::class.java)
         startActivity(intent)
     }
 
