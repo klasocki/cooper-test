359eff5c7b2c98b3dccb7e6a213ee3bcc662b868
klasocki <karolasocki@gmail.com>
2019-11-28 15:49:35 +0200

    Change the main screen view and colors Not final yet

--- app/src/main/java/com/example/coopertest/MainActivity.kt
+++ app/src/main/java/com/example/coopertest/MainActivity.kt
@@ -3,10 +3,8 @@ package com.example.coopertest
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,12 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     fun startTest(v: View) {
