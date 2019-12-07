20e9aec7e802c99a8bf6381a4203fe97f1c68ecd
klasocki <karolasocki@gmail.com>
2019-12-08 00:08:02 +0200

    Style the info text with html

--- app/src/main/java/com/example/coopertest/InfoActivity.kt
+++ app/src/main/java/com/example/coopertest/InfoActivity.kt
@@ -1,13 +1,22 @@
 package com.example.coopertest
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.Html.FROM_HTML_MODE_LEGACY
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.content_info.*
 
 class InfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+        if (Build.VERSION.SDK_INT >= 24) {
+            testInfoView.text = Html.fromHtml(getString(R.string.info_text), FROM_HTML_MODE_LEGACY)
+        } else {
+            testInfoView.text = Html.fromHtml(getString(R.string.info_text))
+        }
     }
 
 }
