79106fbb17d6761fa64757e9e18f43a41501783d
klasocki <karolasocki@gmail.com>
2019-12-07 23:29:08 +0200

    Add InfoActivity providing basic info to the user

---
+++ app/src/main/java/com/example/coopertest/InfoActivity.kt
@@ -0,0 +1,13 @@
+package com.example.coopertest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+
+}
