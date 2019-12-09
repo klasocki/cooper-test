9acd06ecbe9a9e8f609fb6be46aeb144a0486087
klasocki <karolasocki@gmail.com>
2019-12-09 23:21:14 +0200

    Choose default theme based on system settings

--- app/src/main/java/com/lasockiquenon/coopertest/BaseThemedActivity.kt
+++ app/src/main/java/com/lasockiquenon/coopertest/BaseThemedActivity.kt
@@ -2,33 +2,46 @@ package com.lasockiquenon.coopertest
 
 import android.annotation.SuppressLint
 import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
 
 @SuppressLint("Registered")
 open class BaseThemedActivity : AppCompatActivity() {
-    private var darkThemeOn = true
+    private var isDefaultThemeDark = true
+    private var darkThemeOn = isDefaultThemeDark
     private lateinit var sharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
-        darkThemeOn = sharedPref.getBoolean("dark_theme", true)
+        darkThemeOn = getThemeFromSettings()
         setAppTheme(darkThemeOn)
     }
 
     override fun onResume() {
         super.onResume()
-        val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
-        val newTheme = sharedPref.getBoolean("dark_theme", true)
-        if (newTheme != darkThemeOn)
+        val newTheme = getThemeFromSettings()
+        if (newTheme != darkThemeOn) {
             recreate()
+            darkThemeOn = newTheme
+        }
     }
+
     private fun setAppTheme(darkThemeOn: Boolean) {
         if (darkThemeOn)
             setTheme(R.style.DarkTheme)
         else
             setTheme(R.style.LightTheme)
+    }
+
+    private fun getThemeFromSettings(): Boolean {
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> isDefaultThemeDark = true
+            Configuration.UI_MODE_NIGHT_NO -> isDefaultThemeDark = false
+        }
+
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
+        return sharedPref.getBoolean("dark_theme", isDefaultThemeDark)
     }
 }
